e5b2dc2e4ddb8804d075dd5ea220520d90ad766c
zazazaza4 <6715414@gmail.com>
2022-08-21 21:28:33 +0300

    fixed some bags of login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,25 +1,18 @@
 import { signInWithGoogle } from '../../firebase';
 import googleIcon from '../../assets/google.svg';
-import { useEffect, useState } from 'react';
 
 import './Login.scss';
 
 const Login = () => {
-  const [isLoading, setIsLoading] = useState(false);
-
   const singIn = () => {
     signInWithGoogle().catch((error) => {
       console.log(error);
     });
   };
 
-  useEffect(() => {
-    setIsLoading(true);
-  }, []);
-
   return (
     <div className="login">
-      <button disabled={isLoading} className="login__button" onClick={singIn}>
+      <button className="login__button" onClick={singIn}>
         <img className="login__icon" src={googleIcon} alt=""></img>
         <span className="login__text">Sign in with google</span>
       </button>
